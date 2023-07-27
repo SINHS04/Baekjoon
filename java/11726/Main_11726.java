be9d21a6347a371b0821e7e741350b0e73a00263
SINHS04 <102716326+SINHS04@users.noreply.github.com>
2023-07-27 22:50:06 +0900

    commit

--- java/11726/Main_11726.java
+++ java/11726/Main_11726.java
@@ -7,12 +7,26 @@ public class Main_11726 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
-    int[] arr = new int[1001];
-    arr[1] = 1;
-    arr[2] = 2;
-    for (int i = 3; i < arr.length; ++i) {
-      arr[i] = (arr[i - 2] + arr[i - 1]) % 10007;
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    switch (n) {
+      case 1:
+        c = a;
+        break;
+      case 2:
+        c = b;
+        break;
+      case 3:
+        break;
+      default:
+        for (int i = 4; i <= n; ++i) {
+          a = b;
+          b = c;
+          c = (a + b) % 10007;
+        }
+        break;
     }
-    System.out.println(arr[n]);
+    System.out.println(c);
   }
 }
