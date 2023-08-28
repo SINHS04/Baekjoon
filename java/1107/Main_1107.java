af1a92d413d200fe20b8e3d09b910b425c2752f6
SINHS04 <102716326+SINHS04@users.noreply.github.com>
2023-08-28 16:30:18 +0900

    commit

--- java/1107/Main_1107.java
+++ java/1107/Main_1107.java
@@ -7,18 +7,49 @@ public class Main_1107 {
 
   static int N;
   static int num;
-  static int[] button;
+  static boolean[] button;
+  static int curr;
+  static int min;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     N = Integer.parseInt(br.readLine());
     num = Integer.parseInt(br.readLine());
+    curr = 100;
+    min = Math.abs(N - curr);
+    button = new boolean[10];
     if (num != 0) {
-      button = new int[num];
       StringTokenizer st = new StringTokenizer(br.readLine());
-      for (int i = 0; i < num; ++i) {
-        button[i] = Integer.parseInt(st.nextToken());
+      while (st.hasMoreTokens()) {
+        button[Integer.parseInt(st.nextToken())] = true;
       }
+    }
+    if (N == curr) {
+      min = 0;
+    } else if (N < curr) {
+      for (int i = 99; i >= 0; --i) {
+        check(i);
+      }
+    } else {
+      for (int i = 101; i < 999999; ++i) {
+        check(i);
+      }
+    }
+    System.out.println(min);
+  }
+
+  public static void check(int n) {
+    String str = String.valueOf(n);
+    boolean buttonPress = true;
+    for (int j = 0; j < str.length(); ++j) {
+      if (button[str.charAt(j) - '0']) {
+        buttonPress = false;
+        break;
+      }
+    }
+    if (buttonPress) {
+      min = Math.min(min, Math.abs(N - n) + str.length());
+      return;
     }
   }
 }
